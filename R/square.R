4535bee23708351355ab833c1242fe17b0509735
Vincenzo Coia <vincenzo.coia@gmail.com>
2017-11-21 00:14:07 -0800

    Add cube function to `square` family.

--- R/square.R
+++ R/square.R
@@ -1,10 +1,10 @@
-#' Square a vector
+#' Square or Cube a vector
 #'
-#' That's it -- this function just squares a vector.
+#' That's it -- these functions just square or cube a vector.
 #'
-#' @param x The vector to be squared.
+#' @param x The vector to be squared or cubed.
 #'
-#' @return A vector that is the square of \code{x}.
+#' @return A vector that is the square (for \code{square}) or cube (for \code{cube}) of \code{x}.
 #'
 #' @details
 #' This function isn't complicated.
@@ -14,12 +14,19 @@
 #'
 #' Here are some reasons why putting a list in this section is excessive:
 #' \itemize{
-#'      \item This \code{square} function is quite simple.
-#'      \item There's nothing else to say about \code{square}.
+#'      \item These functions are quite simple.
+#'      \item There's nothing else to say about these functions.
 #' }
 #'
+#' If you really want to see more, check out the internal \code{\link{pow}}
+#' function that these functions depend on.
 #' @examples
-#' square(1:10)
+#' cube(1:10)
 #' square(-5)
+#' @rdname square
 #' @export
 square <- function(x) pow(x, a=2)
+
+#' @rdname square
+#' @export
+cube <- function(x) pow(x, a=3)
